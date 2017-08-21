c3603feb647b03fbef228cb15f1ca8eddb48e29e
Roger Ye <shugaoye@yahoo.com>
2017-08-21 16:51:00 +0800

    Enabled PXE boot.

--- utils/test_qemu.sh
+++ utils/test_qemu.sh
@@ -5,12 +5,18 @@ QEMU_PATH=${ANDROID_IMAGE_PATH}/qemu/build/x86_64-softmmu
 ${QEMU_PATH}/qemu-system-x86_64 \
     -enable-kvm \
 	-m 1024 \
+	-boot n \
 	-serial stdio \
-	-netdev user,id=mynet,hostfwd=tcp::5400-:5555 -device virtio-net-pci,netdev=mynet \
+	-netdev user,tftp=/home/aosp/TFTP/,bootfile=tftp://10.0.2.2/pxelinux.0,id=mynet,hostfwd=tcp::5400-:5555 \
+	-device virtio-net-pci,netdev=mynet \
 	-device virtio-mouse-pci -device virtio-keyboard-pci \
 	-d guest_errors \
-	-cdrom  ${ANDROID_IMAGE_PATH}/android-x86_64-6.0-r3.iso\
-	-device VGA -spice port=5900,disable-ticketing
+	-monitor telnet:127.0.0.1:1234,server,nowait \
+	-device virtio-gpu-pci,virgl -spice port=5900,disable-ticketing
 
+#	-tftp /home/aosp/TFTP/ \
+#	-bootp tftp://10.0.2.2/pxelinux.0 \
+#	-cdrom  ${ANDROID_IMAGE_PATH}/android-x86_64-6.0-r3.iso\
 #	-device virtio-gpu-pci,virgl -spice port=5900,disable-ticketing
+#	-device VGA -spice port=5900,disable-ticketing
 
