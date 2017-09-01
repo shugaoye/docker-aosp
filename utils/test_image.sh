369698a621a07ca50377e0d76e275a01b76fc85c
Roger Ye <shugaoye@yahoo.com>
2017-09-01 21:59:40 +0800

    Changed utils/test_image.sh.

--- utils/test_image.sh
+++ utils/test_image.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-VOL1=${HOME}/vol1
-VOL2=${HOME}/.ccache
+[ -e ${VOL1} ] && echo ${VOL1} || VOL1=${HOME}/vol1
+[ -e ${VOL2} ] && echo ${VOL2} || VOL2=${HOME}/.ccache
 USER_ID=`id -u`
 GROUP_ID=`id -g`
 
 if [ -n "$1" ]; then
-	IMAGE=shugaoye/docker-aosp:$1
+	IMAGE=shugaoye/$1
 else
-	IMAGE=shugaoye/aosp:ubuntu16.04-qemu_android
+	IMAGE=shugaoye/docker-spice:latest
 fi
 docker run --privileged -v "${VOL1}:/home/aosp" -v "${VOL2}:/tmp/ccache" -it -e USER_ID=${USER_ID} -e GROUP_ID=${GROUP_ID} ${IMAGE} /bin/bash
